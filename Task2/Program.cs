10f685ede9b2cb55443d1a8aec47a44ff9699566
Daniyal~ <danialastrachan@gmail.comgit config --global user.email danialastrachan@gmail.com>
2022-11-11 11:15:33 +0300

    TAsk2

---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// [1,7] -> такого числа в массиве нет ([1,7] это позиция элемента)
+
+Console.Write("Enter the string: ");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Enter the column: ");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+int n = 5; 
+int m = 7; 
+Random random = new Random();
+int[,] arr = new int[n, m];
+Console.WriteLine("Source array: ");
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+{
+    arr[i, j] = random.Next(10, 99);
+Console.Write("{0} ", arr[i, j]);
+}
+Console.WriteLine();
+}
+    if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+{
+Console.WriteLine("Element does not exist  ");
+}
+    else
+{
+    Console.WriteLine("Array element value = {0}", arr[pos1, pos2]);
+}
+Console.ReadLine();
